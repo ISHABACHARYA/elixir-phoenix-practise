42e9a9fe0da1c6f8fdf8bef1966c5169b8e29597
ISHABACHARYA <ishabachary@gmail.com>
2022-07-24 13:06:42 +0545

    using generator for context, schema, routes, templates and views generator

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -15,4 +15,18 @@ defmodule RumblWeb.Auth do
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
+
+  import Phoenix.Controller
+  alias RumblWeb.Router.Helpers, as: Routes
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access the page.")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
